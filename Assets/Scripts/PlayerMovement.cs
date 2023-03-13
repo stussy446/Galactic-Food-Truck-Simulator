fba3c5d79a544055247035b83c05eefd65c6d653
Steve Rector <steverector27@gmail.com>
2023-03-13 15:34:53 -0700

    Add horizontal movement to player based on configurable speed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Configs")]
+    [SerializeField] CharacterController characterController;
+    [SerializeField] float movementSpeed = 11f;
+
     Vector2 horizontalInput;
+
+    private void Update()
+    {
+        Vector3 horizontalVelocity = CalculateHorizontalVelocity();
+        characterController.Move(horizontalVelocity * Time.deltaTime);
+    }
 
     /// <summary>
     /// Takes in a vector2 value and sets it to the horizontalInput field for use
@@ -14,4 +24,17 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = receivedHorizontalInput;
     }
+
+    /// <summary>
+    /// Calculates the horizontal velocity to be used for movement using the horizontal input and movement speed 
+    /// </summary>
+    /// <returns>Vector3 representing the calculated velocity</returns>
+    private Vector3 CalculateHorizontalVelocity()
+    {
+        Vector3 xVelocity = transform.right * horizontalInput.x;
+        Vector3 yVelocity = transform.forward * horizontalInput.y;
+
+        return (xVelocity + yVelocity) * movementSpeed;
+    }
+
 }
