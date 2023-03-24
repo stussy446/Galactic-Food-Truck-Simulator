9336f011e45b2f71fc89081263b2274f9fcc483e
Owen Lin <lin.owen@gmail.com>
2023-03-24 12:00:44 -0700

    reworked translator functions
    -moved text object updates to TranslateButton
    -disable dial when no order has been received
    -translate function now receives 2 int to set up and test button passes 2 random ints

--- Assets/Scripts/Translatomatic/TranslateButton.cs
+++ Assets/Scripts/Translatomatic/TranslateButton.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TranslateButton : MonoBehaviour
 {
+
+    [SerializeField] private TMP_Text inputText;
+    [SerializeField] private TMP_Text outputText;
+
     [SerializeField] private Vector3 dialAngle;
     private Quaternion originalRotation;
+    private bool buttonEnable = false;
 
     private void OnEnable()
     {
@@ -18,14 +24,29 @@ public class TranslateButton : MonoBehaviour
     }
     void OnMouseDown()
     {
+        if(!buttonEnable)
+        {
+            return;
+        }
+
         transform.Rotate(dialAngle);
         TranslateActions.OnDialClicked();
     }
 
     private void SetDial(TranslatorFunction translate)
     {
+        buttonEnable = true;
         transform.rotation = originalRotation;
         transform.Rotate(dialAngle * translate.GetLanguageID());
+    }
+
+    public void SetInputText(string textString)
+    {
+        inputText.text = textString;
+    }
+    public void SetOutputText(string textString)
+    {
+        outputText.text = textString;
     }
 
     private void OnDisable()
