c024dc6e1e867122442a860ff23accecaf3d2696
Owen Lin <lin.owen@gmail.com>
2023-04-04 14:03:53 -0700

    comment and tweak translator scripts
    -removed unused test scripts

--- Assets/Scripts/Translatomatic/TranslateButton.cs
+++ Assets/Scripts/Translatomatic/TranslateButton.cs
@@ -13,15 +13,20 @@ public class TranslateButton : MonoBehaviour
     private Quaternion originalRotation;
     private bool buttonEnable = false;
 
+    //Listen for order received
     private void OnEnable()
     {
         TranslateActions.OnNewOrder += SetDial;
     }
+
+    //Set up dial segments based on amount of different languages set in database
     private void Start()
     {
         dialAngle.z = 360 / (SqliteScript.GetSize("LangID", "LangIndex"));
         originalRotation = transform.rotation;
     }
+
+    //rotate button and triggers action listend to by translator functions class
     void OnMouseDown()
     {
         if(!buttonEnable)
@@ -33,6 +38,7 @@ public class TranslateButton : MonoBehaviour
         TranslateActions.OnDialClicked();
     }
 
+    //Set initial rotation of dial and default visual output for a given order id
     private void SetDial(TranslatorFunction translate)
     {
         buttonEnable = true;
@@ -40,15 +46,19 @@ public class TranslateButton : MonoBehaviour
         transform.Rotate(dialAngle * translate.GetLanguageID());
     }
 
+    //Set the text of the translator input text box
     public void SetInputText(string textString)
     {
         inputText.text = textString;
     }
+
+    //Set the text of the translator output text box
     public void SetOutputText(string textString)
     {
         outputText.text = textString;
     }
 
+    //Stop listening to actions
     private void OnDisable()
     {
         TranslateActions.OnNewOrder -= SetDial;
