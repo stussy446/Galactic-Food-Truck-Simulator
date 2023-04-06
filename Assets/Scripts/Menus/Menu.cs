f3fbe3165222f0c56c63a7a126b41930e10e515c
Steve Rector <steverector27@gmail.com>
2023-04-06 12:38:08 -0700

    remove extra main camera from main scene and remove camera remova through code
    Also add docstrings to any methods without them in the Menus scritps and Player Controller Scripts

--- Assets/Scripts/Menus/Menu.cs
+++ Assets/Scripts/Menus/Menu.cs
@@ -4,16 +4,25 @@ public class Menu : MonoBehaviour
 {
     [SerializeField] MenuType menuType;
 
+    /// <summary>
+    /// returns the type of menu the Menu object is representing 
+    /// </summary>
     public MenuType MenuType
     {
         get { return menuType; }
     }
 
+    /// <summary>
+    /// Deactivates menu
+    /// </summary>
     public void DeactivateMenu()
     {
         gameObject.SetActive(false);
     }
 
+    /// <summary>
+    /// activates menu
+    /// </summary>
     public void ActivateMenu()
     {
         gameObject.SetActive(true);
