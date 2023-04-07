c70dfac86e6065f5a5c3cf17168d3de61593943b
Leonardo de Andrade <leoandradetv@gmail.com>
2023-04-07 17:11:12 -0400

    Implemented Stress Level
    - Stress Level grows and speed depends on how many bug are in the scene
    - Decreases if player is able to get all the bugs out

--- Assets/Scripts/Pest/Stomping.cs
+++ Assets/Scripts/Pest/Stomping.cs
@@ -6,6 +6,10 @@ public class Stomping : MonoBehaviour
 {
     private const string PLAYER_TAG = "Player";
 
+    /// <summary>
+    /// Destroys the bug when in contact with the player
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag(PLAYER_TAG))
