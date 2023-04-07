e4f248bdb8522139c4de1c6ed6922d7b8cbc14c5
Steve Rector <steverector27@gmail.com>
2023-04-07 16:33:32 -0700

    Import EZ Camershake and add camera shake when stomping bug

--- Assets/Scripts/Pest/Stomping.cs
+++ Assets/Scripts/Pest/Stomping.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EZCameraShake;
 
 public class Stomping : MonoBehaviour
 {
     private const string PLAYER_TAG = "Player";
+    [SerializeField] float shakeMagnitude;
+    [SerializeField] float shakeRoughness;
+    [SerializeField] float shakeFadeInTime;
+    [SerializeField] float shakeFadeOutTime;
 
     /// <summary>
     /// Destroys the bug when in contact with the player
@@ -14,6 +19,7 @@ public class Stomping : MonoBehaviour
     {
         if (other.gameObject.CompareTag(PLAYER_TAG))
         {
+            CameraShaker.Instance.ShakeOnce(shakeMagnitude, shakeRoughness, shakeFadeInTime, shakeFadeOutTime);
             Destroy(gameObject);
         }
     }
