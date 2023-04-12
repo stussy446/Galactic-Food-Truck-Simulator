ddbe0586b2eec7325155d8542c3e6cff25085da4
Steve Rector <steverector27@gmail.com>
2023-04-12 12:38:52 -0700

    Make Settings Panel a Prefab, add some basic styling
    slider color, background, color, etc

--- Assets/Scripts/Menus/Settings.cs
+++ Assets/Scripts/Menus/Settings.cs
@@ -6,6 +6,13 @@ public class Settings : MonoBehaviour
     [SerializeField] AudioSource backgroundAudio;
     [SerializeField] Slider volumeSlider;
 
+    private void Awake()
+    {
+        if(backgroundAudio == null)
+        {
+            Debug.LogError("Missing background Audio, please add appropriate audiosource to Settings script in the Settings Panel game object");
+        }
+    }
     public void AdjustVolume()
     {
         backgroundAudio.volume = volumeSlider.value;
