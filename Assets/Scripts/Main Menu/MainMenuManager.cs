e819157ced5ad623ed7bd80aaa5369887b63d013
Steve Rector <steverector27@gmail.com>
2023-04-12 11:50:25 -0700

    Add Setting Panel that can exit back to main menu
    no logic setup yet in panel itself

--- Assets/Scripts/Main Menu/MainMenuManager.cs
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private Button startGameButton;
     [SerializeField] private Button settingsButton;
     [SerializeField] private Button quitGameButton;
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private TMP_Text titleText;
+    [SerializeField] private GameObject menuButtons;
 
     public void StartGame()
     {
@@ -18,12 +23,26 @@ public class MainMenuManager : MonoBehaviour
     public void EnterSettings()
     {
         // TODO: make a settings page
-        Debug.Log("We'll make a settings page eventually");
+        settingsPanel.SetActive(true);
+        menuButtons.SetActive(false);
+        ToggleTitle(false);
+    }
+
+    public void ExitSettings()
+    {
+        settingsPanel.SetActive(false);
+        ToggleTitle(true);
+        menuButtons.SetActive(true);
     }
 
     public void QuitGame()
     {
         // TODO: Quit Game
         Application.Quit();
+    }
+
+    private void ToggleTitle(bool toogle)
+    {
+        titleText.enabled = toogle;
     }
 }
