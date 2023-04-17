deba38e1a68998d9ad60ed6272f3772479d8bae4
Leonardo de Andrade <leoandradetv@gmail.com>
2023-04-17 17:27:10 -0400

    Adjusted difficulty over time
    Game gets progressively more difficult over time

--- Assets/Scripts/Customers/WaitingInLineState.cs
+++ Assets/Scripts/Customers/WaitingInLineState.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class WaitingInLineState : CustomerBaseState
 {
@@ -11,13 +12,14 @@ public class WaitingInLineState : CustomerBaseState
     private float currentTime;
     private float startingTime;
     private GameObject alienCustomer;
-
+    private float customerCountdownStartTime;
 
     public override void EnterState(CustomerStateManager customerState)
     {
         // TO DO:  set all the information of this instance to the scriptable objects values 
 
-        startingTime = customerState.customer.CustomerCountdownStartTime;
+        customerCountdownStartTime = Random.Range(5f, 10f);
+        startingTime = customerCountdownStartTime;
         alienCustomer = customerState.customer.CustomerPrefab;
         currentTime = startingTime;
         customerPos = alienCustomer.transform.position;
@@ -32,14 +34,14 @@ public class WaitingInLineState : CustomerBaseState
         }
         customerState.customer.OnCustomerEnter();
 
-        
+
         Debug.Log("JELLY ENTITY IS AWAITING IN CUE.");
     }
 
     public override void UpdateState(CustomerStateManager customerState)
     {
         currentTime = startingTime -= Time.deltaTime;
-        if(currentTime <= 0)
+        if (currentTime <= 0)
         {
             ExitState(customerState);
         }
