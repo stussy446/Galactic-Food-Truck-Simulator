4d2b8f82bceb718391c6134dba3004d59a0c1eed
Molly Carroll <72821176+spacemollusk@users.noreply.github.com>
2023-05-04 23:29:11 -0500

    adjusted position and intensity

--- Assets/MollySandbox/Scripts/AudioData.cs
+++ Assets/MollySandbox/Scripts/AudioData.cs
@@ -7,10 +7,12 @@ public class AudioData : MonoBehaviour
 {
     public static AudioSource bossAIAudioSource;
 
-    public static float[] audioSamples = new float[64];
+    public static float[] audioSamples = new float[512];
     public static float[] frequencySection = new float[8];
 
+    public static float[] frequencyBandBuffer = new float[8];
 
+    private float[] buffer = new float[8];
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +25,7 @@ public class AudioData : MonoBehaviour
     {
         GetAudioSource();   
         CreateFrequencySection();
+        BufferFrequencyBand();
     }
 
     private void GetAudioSource()
@@ -32,5 +35,42 @@ public class AudioData : MonoBehaviour
 
     private void CreateFrequencySection()
     {
+        int index = 0;
+        for(int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+            if(i == 7)
+            {
+                sampleCount += 2;
+                
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += audioSamples[index] * (index + 1);
+                index++;
+            }
+            average /= index;
+
+            frequencySection[i] = average * 10;
+        }
+    }
+
+    private void BufferFrequencyBand()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            if (frequencySection[i] > frequencyBandBuffer[i])
+            {
+                frequencyBandBuffer[i] = frequencySection[i];
+                buffer[i] = 0.005f;
+            }
+            if (frequencySection[i] < frequencyBandBuffer[i])
+            {
+                frequencyBandBuffer[i] -= buffer[i];
+                buffer[i] *= 1.2f;
+            }
+        }
     }
 }
